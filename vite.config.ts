c9abd980ddae0c6c47d50124d6df2d9042adcd59
Filip <webisko@duck.com>
2026-01-01 19:11:06 +0100

    Change build output to docs for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/multiserwis-kutno/',
       server: {
         port: 3000,
         host: '0.0.0.0',
+      },
+      build: {
+        outDir: 'docs'
       },
       plugins: [react()],
       define: {
